6b01691057f9b2c7a42b12cc3a5b110b9e236616
Ma Xueyuan <machinemxy@me.com>
2021-01-10 23:22:47 +0900

    add project, update gitignore

---
+++ TRPG2/TRPG2App.swift
@@ -0,0 +1,17 @@
+//
+//  TRPG2App.swift
+//  TRPG2
+//
+//  Created by 馬学渊 on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct TRPG2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
